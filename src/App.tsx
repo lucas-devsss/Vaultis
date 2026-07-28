de762282a158907fa0a92d21bfcd44fa447b0048
Caio Lucas <clucaszrtmain@gmail.com>
2026-07-28 01:41:41 -0300

    feat: Add the route of the CatalogCharacters component and an Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
 // import { useState } from 'react'
-import { BrowserRouter, Link, Outlet, Route, Routes } from "react-router";
-import { Undo2 } from "lucide-react";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router";
+import { CatalogCharacters } from "./pages/CatalogCharacters";
 function App() {
   return (
     <>
       <BrowserRouter>
-        <div className="pl-2">
-          <p className="text-2xl">Oi amigos, bem vindo ao Vaultis {":)"}</p>
-          <Link className="text-blue-800" to={"/"}>
-            Voltar rota <Undo2 size={"25px"} />
-          </Link>
-          <Link className="text-red-800" to={"rota1"}>
-            Rota1
-          </Link>
-        </div>
-
-        <Outlet />
+        <Outlet></Outlet>
 
         <Routes>
-          <Route path="rota1" element={<h1>Você mudou a rota, parabéns</h1>} />
+          <Route path="/" Component={CatalogCharacters}></Route>
         </Routes>
       </BrowserRouter>
     </>
