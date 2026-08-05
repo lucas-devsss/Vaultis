c6354e9e1879990b273304a42529a325416e3829
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-05 03:31:31 -0300

    feat: create the getFetchMore function to call more characters

--- src/services/useFetch.tsx
+++ src/services/useFetch.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function useFetch() {
   const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
   async function getFetch(range: number) {
     try {
       setLoading(true);
@@ -17,5 +18,21 @@ export default function useFetch() {
       setLoading(false);
     }
   }
-  return [getFetch, loading];
+  async function getFetchMore(range: number) {
+    try {
+      setLoadingMore(true);
+      const apiKey = import.meta.env.VITE_API_TOKEN;
+      const response = await fetch(`api/api.php/${apiKey}/${range}`);
+      const data = await response.json();
+      if (data.response === "success") {
+        return data;
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoadingMore(false);
+    }
+  }
+
+  return [getFetch, loading, getFetchMore, loadingMore];
 }
