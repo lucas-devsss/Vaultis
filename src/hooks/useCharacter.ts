7d934e0dd8e8b4fd5a87e0ecb0ecba7041c5475f
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-06 03:15:14 -0300

    refactor: create the useCharacter custom hook and call all the necessary states and functions in catalogCharacters.

---
+++ src/hooks/useCharacter.ts
@@ -0,0 +1,58 @@
+import { useState, useRef } from "react";
+import useFetchCharacters from "../services/useFetchCharacters";
+import type { Characters } from "../types/CharacterTypes";
+
+export default function useCharacter() {
+  const [charactersData, setCharactersData] = useState<Characters[]>([]);
+  const { getFetchCharacters, loading, getFetchMoreCharacters, loadingMore } =
+    useFetchCharacters();
+  const [isFetched, setFetched] = useState(false);
+  const nextId = useRef(20);
+  const initialC = useRef([
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ]);
+  const isFetching = useRef(false);
+
+  async function getCharacters() {
+    if (typeof getFetchCharacters === "function") {
+      const data = await Promise.all(
+        initialC.current.map((id) => getFetchCharacters(id)),
+      );
+      setCharactersData(data);
+      setFetched(true);
+    }
+  }
+
+  async function getMoreCharacters() {
+    if (typeof getFetchMoreCharacters === "function") {
+      const data = await Promise.all(
+        initialC.current.map((id) => getFetchMoreCharacters(id)),
+      );
+      setCharactersData((prev) => [...prev, ...data]);
+    }
+  }
+
+  function setIsFetching(value: boolean) {
+    isFetching.current = value;
+  }
+
+  function loadNextIds(value: number) {
+    initialC.current = [];
+    for (let i = nextId.current + 1; i <= nextId.current + value; i++) {
+      initialC.current.push(i);
+    }
+    nextId.current = initialC.current[initialC.current.length - 1];
+  }
+
+  return {
+    charactersData,
+    loading,
+    loadingMore,
+    isFetched,
+    isFetching,
+    getCharacters,
+    getMoreCharacters,
+    setIsFetching,
+    loadNextIds,
+  };
+}
--- src/pages/CatalogCharacters.tsx
+++ src/pages/CatalogCharacters.tsx
@@ -1,29 +1,24 @@
-import { useEffect, useState } from "react";
-import useFetch from "../services/useFetch";
+import { useEffect } from "react";
 import CardCharacter from "../components/CardCharacter";
-import type { Characters } from "../types/CharacterTypes";
 import { useRef } from "react";
+import useCharacter from "../hooks/useCharacter";
 
 export function CatalogCharacters() {
-  const [dataState, setDataState] = useState<Characters[]>([]);
-  const [getFetch, loading, getFetchMore, loadingMore] = useFetch();
-  const [isFetched, setFetched] = useState(false);
-  const targetRef = useRef<null | HTMLDivElement>(null);
-  const nextId = useRef(20);
-  const initialC = useRef([
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  ]);
-  const isFetching = useRef(false);
+  const {
+    loading,
+    loadingMore,
+    charactersData,
+    isFetched,
+    isFetching,
+    getCharacters,
+    getMoreCharacters,
+    setIsFetching,
+    loadNextIds,
+  } = useCharacter();
+
+  const targetRef = useRef(null);
+
   useEffect(() => {
-    const getCharacters = async () => {
-      if (typeof getFetch === "function") {
-        const data = await Promise.all(
-          initialC.current.map((id) => getFetch(id)),
-        );
-        setDataState(data);
-        setFetched(true);
-      }
-    };
     getCharacters();
   }, []);
 
@@ -31,28 +26,16 @@ export function CatalogCharacters() {
     const observer = new IntersectionObserver(
       (entries) => {
         if (entries[0].isIntersecting && !isFetching.current) {
-          isFetching.current = true;
-          initialC.current = [];
-          for (let i = nextId.current + 1; i <= nextId.current + 10; i++) {
-            initialC.current.push(i);
-          }
-          nextId.current = initialC.current[initialC.current.length - 1];
+          setIsFetching(true);
+          loadNextIds(10);
           getMoreCharacters().then(() => {
-            isFetching.current = false;
+            setIsFetching(false);
           });
         }
       },
       { threshold: 0.1 },
     );
 
-    const getMoreCharacters = async () => {
-      if (typeof getFetchMore === "function") {
-        const data = await Promise.all(
-          initialC.current.map((id) => getFetchMore(id)),
-        );
-        setDataState((prev) => [...prev, ...data]);
-      }
-    };
     if (targetRef.current) {
       observer.observe(targetRef.current);
     }
@@ -69,7 +52,7 @@ export function CatalogCharacters() {
   return (
     <>
       <div className="grid gap-2.5 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] px-8 ">
-        {dataState.map((a) => (
+        {charactersData.map((a) => (
           <CardCharacter
             key={a.id}
             name={a.name}
