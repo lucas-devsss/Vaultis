394d26c3a60eda0a829543ef76145c845b653d2d
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-06 23:30:06 -0300

    feat: create an array to render 20 skeleton cards as a loading before the API be requested, wrap the whole return in a section and adds a complementary text above the container card

--- src/pages/CatalogCharacters.tsx
+++ src/pages/CatalogCharacters.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import CardCharacter from "../components/CardCharacter";
 import { useRef } from "react";
 import useCharacter from "../hooks/useCharacter";
+import SkeletonCard from "../components/SkeletonCard";
 
 export function CatalogCharacters() {
   const {
@@ -22,6 +23,10 @@ export function CatalogCharacters() {
     getCharacters();
   }, []);
 
+  const skelCardsRange = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       (entries) => {
@@ -35,7 +40,6 @@ export function CatalogCharacters() {
       },
       { threshold: 0.1 },
     );
-
     if (targetRef.current) {
       observer.observe(targetRef.current);
     }
@@ -47,29 +51,38 @@ export function CatalogCharacters() {
   }, [isFetched]);
 
   if (loading) {
-    return <p>Carregando...</p>;
+    return (
+      <div className="bg-slate-900 px-8 py-2 h-full min-h-dvh grid gap-2.5 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] ">
+        {skelCardsRange.map((a) => (
+          <SkeletonCard key={a} />
+        ))}
+      </div>
+    );
   }
   return (
     <>
-      <div className="grid gap-2.5 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] px-8 ">
-        {charactersData.map((a) => (
-          <CardCharacter
-            key={a.id}
-            name={a.name}
-            id={a.id}
-            biography={a.biography}
-            image={a.image}
-            connections={a.connections}
-            appearance={a.appearance}
-          ></CardCharacter>
-        ))}
-      </div>
-      {loadingMore && (
-        <div className="w-25 h-25">
-          <p>Carregando...</p>
+      <section className="px-8 bg-slate-900 h-full min-h-dvh">
+        <p className="text-slate-500 font-outfit">Recrutas disponiveis</p>
+        <div className="grid gap-2.5 grid-cols-[repeat(auto-fit,minmax(150px,1fr))] sm:grid-cols-[repeat(auto-fit,minmax(200px,1fr))] ">
+          {charactersData.map((a) => (
+            <CardCharacter
+              key={a.id}
+              name={a.name}
+              id={a.id}
+              biography={a.biography}
+              image={a.image}
+              connections={a.connections}
+              appearance={a.appearance}
+            ></CardCharacter>
+          ))}
         </div>
-      )}
-      <div ref={targetRef} className="w-5 h-5"></div>
+        <div ref={targetRef} className="w-5 h-5"></div>
+        {loadingMore && (
+          <div className="w-25 h-25">
+            <p>Carregando...</p>
+          </div>
+        )}
+      </section>
     </>
   );
 }
