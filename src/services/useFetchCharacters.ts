a1274d8284884b9d122cdbc3b41d67578265190b
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-12 22:28:27 -0300

    feat: Create the getSearchCharacters to search the character info by a name

--- src/services/useFetchCharacters.ts
+++ src/services/useFetchCharacters.ts
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function useFetchCharacters() {
   const [loading, setLoading] = useState(true);
   const [loadingMore, setLoadingMore] = useState(false);
+  const [searchLoading, setSearchLoading] = useState(false);
   const apiKey = import.meta.env.VITE_API_TOKEN;
 
   async function getFetchCharacters(range: number) {
@@ -36,5 +37,26 @@ export default function useFetchCharacters() {
     }
   }
 
-  return { getFetchCharacters, loading, getFetchMoreCharacters, loadingMore };
+  async function getSearchCharacters(value: string) {
+    try {
+      setSearchLoading(true);
+      const response = await fetch(`api/api.php/${apiKey}/search/${value}`);
+      const data = await response.json();
+      if (data.response === "success") {
+        return data;
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setSearchLoading(false);
+    }
+  }
+  return {
+    getFetchCharacters,
+    loading,
+    getFetchMoreCharacters,
+    loadingMore,
+    getSearchCharacters,
+    searchLoading,
+  };
 }
