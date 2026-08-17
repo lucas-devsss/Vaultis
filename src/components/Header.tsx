6952297b71c1e3f97ef87179f2e50f5c93f86787
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-17 01:52:45 -0300

    feat: adjuste the header type

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,11 @@
-export default function HeaderComponent(props) {
-  // console.log(props);
+interface HeaderProps {
+  children: React.ReactNode;
+}
+
+export default function HeaderComponent({ children }: HeaderProps) {
   return (
     <header className="grid justify-between items-center grid-cols-2 sm:grid-flow-row sm:gap-13.5 sm:grid-cols-[auto_minmax(320px,1fr)_auto] gap-6 fixed p-8 bg-slate-800 w-full">
-      {props.children}
+      {children}
     </header>
   );
 }
