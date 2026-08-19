1b165be9e7d386be3d45ac77508fba94a68b1c3a
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-19 03:34:55 -0300

    refactor: remove unused fetch functions and states from useFetchCharacters after api migration

--- src/services/useFetchCharacters.ts
+++ src/services/useFetchCharacters.ts
@@ -2,9 +2,6 @@ import { useState } from "react";
 
 export default function useFetchCharacters() {
   const [loading, setLoading] = useState(true);
-  const [loadingMore, setLoadingMore] = useState(false);
-  const [searchLoading, setSearchLoading] = useState(false);
-  const apiKey = import.meta.env.VITE_API_TOKEN;
 
   async function getFetchCharacters() {
     try {
@@ -23,42 +20,8 @@ export default function useFetchCharacters() {
     }
   }
 
-  async function getFetchMoreCharacters(range: number) {
-    try {
-      setLoadingMore(true);
-
-      const response = await fetch(`api/api.php/${apiKey}/${range}`);
-      const data = await response.json();
-      if (data.response === "success") {
-        return data;
-      }
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setLoadingMore(false);
-    }
-  }
-
-  async function getSearchCharacters(value: string) {
-    try {
-      setSearchLoading(true);
-      const response = await fetch(`api/api.php/${apiKey}/search/${value}`);
-      const data = await response.json();
-      if (data.response === "success") {
-        return data;
-      }
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setSearchLoading(false);
-    }
-  }
   return {
     getFetchCharacters,
     loading,
-    getFetchMoreCharacters,
-    loadingMore,
-    getSearchCharacters,
-    searchLoading,
   };
 }
