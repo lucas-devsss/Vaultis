a89baa66b0fc63e4eda3ec557829759568e4cc90
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-18 22:55:24 -0300

    refactor: pass charactersData as prop to InputHeader removing api dependency

--- src/components/CatalogHeader.tsx
+++ src/components/CatalogHeader.tsx
@@ -1,11 +1,16 @@
+import type { Characters } from "../types/CharacterTypes";
 import HeaderComponent from "./Header";
 import InputHeader from "./InputHeader";
 
-export default function CatalogHeader() {
+interface catalogHeaderProps {
+  charactersData: Characters[];
+}
+
+export default function CatalogHeader({ charactersData }: catalogHeaderProps) {
   return (
     <HeaderComponent>
       <h1 className="font-bebas text-4xl text-white">Vaultis</h1>
-      <InputHeader />
+      <InputHeader charactersData={charactersData} />
       <button className="px-6 py-3 max-w-62.5 text-sm font-outfit uppercase bg-gray-700 text-white box-border border-4 duration-300 rounded-md border-transparent hover:border-red-500 hover:text-red-500 cursor-pointer">
         meus recrutas
       </button>
