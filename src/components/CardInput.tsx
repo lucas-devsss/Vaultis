60ec492d3934fb25c8198586d89cd7682b06cc0d
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-25 23:12:53 -0300

    refactor: replace Ver Dados button with LinkData component in CardInput

--- src/components/CardInput.tsx
+++ src/components/CardInput.tsx
@@ -1,5 +1,5 @@
 import type { images } from "../types/CharacterTypes";
-
+import LinkData from "./LinkData";
 interface CardInputProps {
   name: string;
   images: images;
@@ -7,7 +7,6 @@ interface CardInputProps {
 }
 
 export default function CardInput({ name, images, id }: CardInputProps) {
-  console.log(id);
   return (
     <article className="max-w-225 flex m-2 ">
       <div className="w-50 max-h-70">
@@ -18,9 +17,7 @@ export default function CardInput({ name, images, id }: CardInputProps) {
         <button className="px-6 py-4 font-bebas text-2xl bg-red-700 text-white border-3 border-transparent duration-150 hover:border-yellow-300 cursor-pointer">
           Recrutar
         </button>
-        <button className="px-6 py-4 font-bebas text-xl text-white border-3 border-red-700 hover:bg-yellow-300 hover:text-red-900 hover:border-red-900 duration-150 cursor-pointer">
-          Ver dados
-        </button>
+        <LinkData characterId={id} />
       </div>
     </article>
   );
