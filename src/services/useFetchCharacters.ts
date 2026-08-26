ee73ff36a1bb2f145a7c16940426c965b1bd54fe
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-25 23:06:52 -0300

    feat: add the getCharacterDetails to request a info by ID

--- src/services/useFetchCharacters.ts
+++ src/services/useFetchCharacters.ts
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 export default function useFetchCharacters() {
   const [loading, setLoading] = useState(true);
+  const [loadingId, setLoadingId] = useState(false);
 
   async function getFetchCharacters() {
     try {
@@ -19,9 +20,27 @@ export default function useFetchCharacters() {
       setLoading(false);
     }
   }
+  async function getCharacterDetails(id: string) {
+    try {
+      setLoadingId(true);
+      const response = await fetch(
+        `https://akabab.github.io/superhero-api/api/id/${id}.json`,
+      );
+      const data = await response.json();
+      if (data) {
+        return data;
+      }
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoadingId(false);
+    }
+  }
 
   return {
     getFetchCharacters,
     loading,
+    getCharacterDetails,
+    loadingId,
   };
 }
