dbf261eb06fa304ffbf73a47c2d9f30ab458c8e4
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-25 23:40:48 -0300

    feat: add DetailsCharacter layout and render character details data

--- src/pages/DetailsCharacter.tsx
+++ src/pages/DetailsCharacter.tsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import useFetchCharacters from "../services/useFetchCharacters";
 import type { Characters } from "../types/CharacterTypes";
+import DetailsHeader from "../components/DetailsHeader";
+import AlignmentBadge from "../components/AlignmentBadge";
 
 function DetailsCharacter() {
   const paramsId = useParams();
@@ -33,10 +35,113 @@ function DetailsCharacter() {
     }
     getCharacters();
     getDetails();
-  }, []);
-  console.log(characterDetails);
+  }, [paramsId]);
 
-  return <h1>DetailsCharacter page :)</h1>;
+  console.log(characterDetails);
+  if (loadingId) {
+    return <h1>Is loading...</h1>;
+  }
+  if (characterDetails && characters) {
+    return (
+      <>
+        <DetailsHeader charactersData={characters} />
+        <section className="px-8 py-50 bg-slate-900 min-h-dvh flex flex-col gap-8 md:flex-row">
+          <div className="w-full max-w-112.5 max-h-137.5 self-center md:self-auto">
+            <img
+              className="w-full max-h-full object-cover object-top"
+              src={characterDetails.images.lg}
+              alt={characterDetails.name}
+            />
+          </div>
+          <div className="flex flex-col w-full">
+            <div className="flex items-center flex-col md:flex-row md:justify-between">
+              <p className="font-bebas text-[60px] text-white text-center">
+                {characterDetails.name}
+              </p>
+              <button className="px-6 py-4 font-bebas w-full max-w-50 text-2xl bg-red-700 border-3 border-transparent duration-150 hover:border-yellow-300 cursor-pointer">
+                Recrutar
+              </button>
+            </div>
+            <div className="flex gap-2.5 justify-center items-center md:justify-start">
+              <p className="font-bebas text-[40px] text-slate-200">
+                {characterDetails.biography.publisher}
+              </p>
+              <AlignmentBadge
+                content={characterDetails.biography.alignment}
+                unknownContent="Status desconhecido"
+              />
+            </div>
+            <div className="border-y-2 border-slate-400 mt-2">
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">Codinomes</p>
+                <p className="text-slate-300 text-[20px] mb-1.5 border-b-2 border-slate-400">
+                  {characterDetails.biography.alterEgos}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl ">Gênero</p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.appearance.gender}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">Raça</p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.appearance.race}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">
+                  Nome Completo
+                </p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.biography.fullName}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">
+                  Local de Nascimento
+                </p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.biography.placeOfBirth}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">
+                  Primeira Aparição
+                </p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.biography.firstAppearance}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">
+                  Grupo Afiliado
+                </p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.connections.groupAffiliation}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">
+                  Base de Operações
+                </p>
+                <p className="text-slate-300 text-[20px] mb-2 border-b-2 border-slate-400">
+                  {characterDetails.work.base}
+                </p>
+              </div>
+              <div>
+                <p className="text-slate-500 font-bebas text-2xl">Trabalhos</p>
+                <p className="text-slate-300 text-[20px] mb-2">
+                  {characterDetails.work.occupation}
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+      </>
+    );
+  }
 }
 
 export default DetailsCharacter;
