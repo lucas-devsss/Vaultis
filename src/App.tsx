12b097cc989e43e6a8dfbb6481abad5dc0122c5b
Caio Lucas <clucaszrtmain@gmail.com>
2026-08-28 02:09:45 -0300

    feat: create route to render DetailsCharacter component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 // import { useState } from 'react'
 import { BrowserRouter, Outlet, Route, Routes } from "react-router";
 import { CatalogCharacters } from "./pages/CatalogCharacters";
+import DetailsCharacter from "./pages/DetailsCharacter";
+
 function App() {
   return (
     <>
@@ -9,6 +11,7 @@ function App() {
 
         <Routes>
           <Route path="/" Component={CatalogCharacters}></Route>
+          <Route path=":id" Component={DetailsCharacter} />
         </Routes>
       </BrowserRouter>
     </>
