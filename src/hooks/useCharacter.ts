40a28fecc3c68b722077f752b22c317af72efee3
Caio Lucas <clucaszrtmain@gmail.com>
2026-09-06 16:12:14 -0300

    feat: create the favorites state and functions to add and delete characters from this state

--- src/hooks/useCharacter.ts
+++ src/hooks/useCharacter.ts
@@ -7,6 +7,9 @@ export default function useCharacter() {
   const { getFetchCharacters, loading } = useFetchCharacters();
   const [rangeCharacters, setRangeCharacters] = useState(20);
   const characters = charactersData.slice(0, rangeCharacters);
+  const [favoritesCharacter, setFavoritesCharacter] = useState<Characters[]>(
+    [],
+  );
 
   async function getCharacters() {
     if (typeof getFetchCharacters === "function") {
@@ -19,11 +22,22 @@ export default function useCharacter() {
     setRangeCharacters((prev) => prev + range);
   }
 
+  function addFavoriteCharacter(character: Characters) {
+    setFavoritesCharacter((prev) => [...prev, character]);
+  }
+
+  function removeFavoriteCharacter(id: string) {
+    setFavoritesCharacter((prev) => prev.filter((a) => a.id !== id));
+  }
+
   return {
     charactersData,
     loading,
     getCharacters,
     characters,
     loadMoreCharacters,
+    addFavoriteCharacter,
+    favoritesCharacter,
+    removeFavoriteCharacter,
   };
 }
