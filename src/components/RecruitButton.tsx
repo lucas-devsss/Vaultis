d64283c4bb4bc974a606be8471358b49c498bb94
Caio Lucas <clucaszrtmain@gmail.com>
2026-09-06 19:02:27 -0300

    feat: add RecruitButton component

---
+++ src/components/RecruitButton.tsx
@@ -0,0 +1,31 @@
+interface RecruitButtonProps {
+  onRecruit?: () => void;
+  onDesrecruit?: () => void;
+}
+
+export default function RecruitButton({
+  onRecruit,
+  onDesrecruit,
+}: RecruitButtonProps) {
+  if (onDesrecruit) {
+    return (
+      <button
+        onClick={onDesrecruit}
+        className="px-6 py-4 font-bebas text-white text-xl  bg-red-700 border-3 border-transparent duration-150 hover:border-yellow-300 cursor-pointer"
+      >
+        Desrecrutar
+      </button>
+    );
+  }
+
+  if (onRecruit) {
+    return (
+      <button
+        onClick={onRecruit}
+        className="px-6 py-4 font-bebas text-2xl text-white bg-red-700 border-3 border-transparent duration-150 hover:border-yellow-300 cursor-pointer"
+      >
+        Recrutar
+      </button>
+    );
+  }
+}
