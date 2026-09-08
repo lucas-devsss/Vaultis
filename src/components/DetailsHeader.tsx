40d427532e1fd0b3afd3a7e69dfdd845b99758b0
Caio Lucas <clucaszrtmain@gmail.com>
2026-09-07 21:45:43 -0300

    feat: show favorites count and change the button for a link to recruits route

--- src/components/DetailsHeader.tsx
+++ src/components/DetailsHeader.tsx
@@ -1,17 +1,24 @@
 import { Link } from "react-router";
 import HeaderComponent from "./Header";
 import InputHeader from "./InputHeader";
+import { CharacterContext } from "../context/characterContext";
+import { useContext } from "react";
 
 export default function DetailsHeader() {
+  const { favoritesCharacter } = useContext(CharacterContext);
+
   return (
     <HeaderComponent>
       <Link to={"/"} className="font-bebas text-white text-2xl ">
         Recrutar
       </Link>
       <InputHeader />
-      <button className="px-6 py-3 max-w-62.5 text-sm font-outfit uppercase bg-gray-700 text-white box-border border-4 duration-300 rounded-md border-transparent hover:border-red-500 hover:text-red-500 cursor-pointer">
-        meus recrutas
-      </button>
+      <Link
+        to={"/recruits"}
+        className="px-6 py-3 max-w-62.5 text-sm font-outfit uppercase bg-gray-700 text-white box-border border-4 duration-300 rounded-md border-transparent hover:border-red-500 hover:text-red-500 cursor-pointer"
+      >
+        meus recrutas ({favoritesCharacter.length})
+      </Link>
     </HeaderComponent>
   );
 }
